b28bb7c72bd6b7a04d92d41d9e2d44e268abec00
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2018-10-04 08:40:44 +0300

    It's a better to pass shared_ptr by reference.

--- include/riot/server/specialized/utils/external_auth.hpp
+++ include/riot/server/specialized/utils/external_auth.hpp
@@ -3,4 +3,12 @@
 
 // TODO start implementing it here
 
+namespace riot::server::specialized::utils {
+
+class external_auth_server {
+
+};
+
+};
+
 #endif // RIOT_SERVER_SPECIALIZED_UTILS_EXTERNAL_AUTH_HPP_INCLUDED
